bac306b73673c7c1c60816459a873334487d9377
Julian Eisenmann <1810653115@stud.fh-kufstein.ac.at#>
2018-11-25 15:25:17 +0100

    Aufgabe 4

---
+++ src/Aufgabe4_1810653115.java
@@ -0,0 +1,28 @@
+public class Aufgabe4_1810653115
+{
+    public static void main(String[] args)
+    {
+        //For Schleife
+        for(int h=1; h<11; h++)
+        {
+            System.out.println("Der Schleifenzähler ist: " + h);
+        }
+
+        //While Schleife
+        int i = 1;
+        while(i < 11)
+        {
+            System.out.println("Der Schleifenzähler ist: " + i);
+            i++;
+        }
+
+        int j = 1;
+        do
+        {
+            System.out.println("Der Schleifenzähler ist: " + j);
+            j++;
+        } while(j < 11);
+
+    }
+
+}

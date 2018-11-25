e6642cde9922328d0736b012581d870652832442
Julian Eisenmann <1810653115@stud.fh-kufstein.ac.at#>
2018-11-25 15:07:57 +0100

    Aufgabe 3

---
+++ src/Aufgabe3_1810653115.java
@@ -0,0 +1,68 @@
+import javax.swing.*;
+
+
+public class Aufgabe3_1810653115
+{
+    public static void main(String[] args)
+    {
+        // Fehler bei falsche Eingabe (z.B. Zeichen oder bei leerer Eingabe)
+        // Double
+        String eingabe = JOptionPane.showInputDialog("Bitte Eingabe tätigen! Double");
+        double alter = Double.parseDouble(eingabe);
+        String newValue = String.valueOf(alter);
+
+        // Es können nur Zahlen eingegeben werden, auch mit Kommastellen
+        JOptionPane.showMessageDialog(null, alter);
+
+        // Int
+        String eingabe1 = JOptionPane.showInputDialog("Bitte Eingabe tätigen! Int");
+        int eingabeint;
+        eingabeint = Integer.parseInt(eingabe1);
+
+        JOptionPane.showMessageDialog(null, eingabeint);
+
+        // boolean
+        // Immer false außer bei Eingabe true
+        String eingabe2 = JOptionPane.showInputDialog("Bitte Eingabe tätigen! Boolean");
+        boolean eingabebool;
+        eingabebool = Boolean.parseBoolean(eingabe2);
+
+        JOptionPane.showMessageDialog(null, eingabebool);
+
+        // long
+        String eingabe3 = JOptionPane.showInputDialog("Bitte Eingabe tätigen! Long");
+        long eingabelong;
+        eingabelong = Long.parseLong(eingabe3);
+
+        JOptionPane.showMessageDialog(null, eingabelong);
+
+        // char
+        // Zeichen und Zahlen
+        String eingabe4 = JOptionPane.showInputDialog("Bitte Eingabe tätigen! Char");
+        char eingabechar;
+        eingabechar = eingabe4.charAt(0);
+
+        JOptionPane.showMessageDialog(null, eingabechar);
+
+        // byte
+        String eingabe5 = JOptionPane.showInputDialog("Bitte Eingabe tätigen! Byte");
+        byte eingabebyte;
+        eingabebyte = Byte.parseByte(eingabe5);
+
+        JOptionPane.showMessageDialog(null, eingabebyte);
+
+        // short
+        String eingabe6 = JOptionPane.showInputDialog("Bitte Eingabe tätigen! Short");
+        short eingabeshort;
+        eingabeshort = Short.parseShort(eingabe6);
+
+        JOptionPane.showMessageDialog(null, eingabeshort);
+
+        // float
+        String eingabe7 = JOptionPane.showInputDialog("Bitte Eingabe tätigen! Float");
+        float eingabefloat;
+        eingabefloat = Float.parseFloat(eingabe7);
+
+        JOptionPane.showMessageDialog(null, eingabefloat);
+    }
+}
